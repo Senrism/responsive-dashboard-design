ffc0afce4147b389399bf06bdbae517b0c2fe353
Febrylasena <febryl@thebigrichgroup.com>
2023-08-31 12:05:16 +0700

    first commit

---
+++ order.js
@@ -0,0 +1,21 @@
+// Dummy
+const orders = [
+    {
+        'productName'   : 'Logitech keyboard',
+        'productNumber' : '1002',
+        'paymentStatus' : 'due',
+        'status'        : 'Pending'
+    },
+    {
+        'productName'   : 'Logitech keyboard',
+        'productNumber' : '1002',
+        'paymentStatus' : 'due',
+        'status'        : 'Pending'
+    },
+    {
+        'productName'   : 'Logitech keyboard',
+        'productNumber' : '1002',
+        'paymentStatus' : 'due',
+        'status'        : 'Pending'
+    }
+];
